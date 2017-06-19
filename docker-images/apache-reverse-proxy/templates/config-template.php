bfdcaa073a18762f80aeb4d81ecd35971da1b6c0
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2017-06-19 12:08:30 +0200

    Added ports on config-template

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -8,9 +8,9 @@
    #ErrorLog ${APACHE_LOG_DIR}/error.log
    #CustomLog ${APACHE_LOG_DIR}/access.log combined
 
-   ProxyPass '/api/quotes/' 'http://<?php print "$dynamic_app"?>/'
-   ProxyPassReverse '/api/quotes/' 'http://<?php print "$dynamic_app"?>/'
+   ProxyPass '/api/quotes/' 'http://<?php print "$dynamic_app"?>:3000/'
+   ProxyPassReverse '/api/quotes/' 'http://<?php print "$dynamic_app"?>:3000/'
 
-   ProxyPass '/' 'http://<?php print "$static_app"?>/'
-   ProxyPassReverse '/' '<?php print "$static_app"?>/'
+   ProxyPass '/' 'http://<?php print "$static_app"?>:80/'
+   ProxyPassReverse '/' '<?php print "$static_app"?>:80/'
 </VirtualHost>
